9298471cf08e3a2f8b6bd7cdfeaaf1eed5ec82c8
Waraht Punyarote <waraht_pun@truecorp.co.th>
2020-07-25 14:47:01 +0700

    Applied Partials.

--- index.js
+++ index.js
@@ -1,13 +1,33 @@
 var express = require('express')
-var exphbs = require('express-handlebars')
-
+const fs = require('fs')
+const path = require('path')
+const Handlebars = require('hbs')
 var app = express()
 
-app.engine('handlebars', exphbs())
-app.set('view engine', 'handlebars')
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'hbs')
+
+const header = Handlebars.compile(
+  fs.readFileSync('./views/partials/header.hbs').toString('utf-8')
+)
+
+const body = Handlebars.compile(
+  fs.readFileSync('./views/partials/body.hbs').toString('utf-8')
+)
+
+const footer = Handlebars.compile(
+  fs.readFileSync('./views/partials/footer.hbs').toString('utf-8')
+)
+
+Handlebars.registerPartial('body', body)
+Handlebars.registerPartial('header', header)
+Handlebars.registerPartial('footer', footer)
 
 app.get('/', function (req, res) {
-  res.render('home')
+  res.render('layouts/main')
 })
 
-app.listen(3000)
+const port = process.env.PORT || 5000
+app.listen(port, () => {
+  console.log(`Listening to port: ${port}`)
+})
